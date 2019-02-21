4f68eeb4f72f2c7f54ea89c820bbd7bf4da7ece7
Chris Martin <martintechsolution@gmail.com>
2019-02-21 11:40:13 -0500

    Updated shared workspace

--- SeeFood/AppDelegate.swift
+++ SeeFood/AppDelegate.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import AppCenter
+import AppCenterAnalytics
+import AppCenterCrashes
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        MSAppCenter.start("2fc9fa5e-aa44-406f-96b3-1e79c750cf69", withServices:[ MSAnalytics.self, MSCrashes.self ])
         return true
     }
 
